4dd933265ded4d68a248d479fa16508f68fb2b68
Kross1945 <kross1945@mail.ru>
2014-11-10 20:25:44 +0300

    Формы

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Rect rectangle = new Rect();
+        private Random r = new Random(DateTime.Now.TimeOfDay.Milliseconds);
+
+            
+        public Form1()
+        {
+
+            InitializeComponent();
+            rectangle.X = 50;
+            rectangle.Y = 50;
+            rectangle.Widht = 150;
+            rectangle.Height = 100;
+            rectangle.Color = Color.DeepPink;
+            DoubleBuffered = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            rectangle.Draw(e.Graphics);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (rectangle.IsInsaide(e.X, e.Y))
+            {
+                rectangle.Color = Color.DarkTurquoise;
+            }
+            else
+            {
+                rectangle.Color = Color.DarkRed;
+            }
+            Refresh();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            rectangle.X = r.Next(0, this.Width);
+            rectangle.Y = r.Next(0, this.Height);
+            Refresh();
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            rectangle.Height = r.Next(0, 150);
+            rectangle.Widht = r.Next(0, 150);
+            Refresh();
+        }
+
+        
+    }
+}
---
+++ WindowsFormsApplication1/Rect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Rect
+    {
+        // protected виден в наследниках и классах
+        // privat только в самом классе
+        public int X;
+        public int Y;
+        public int Widht;
+        public int Height;
+        public Color Color;
+
+        public void Draw(Graphics gr)
+        {
+            Pen pen = new Pen(Color, 7);
+            gr.DrawRectangle(pen, X, Y, Widht, Height);
+        }
+
+        public bool IsInsaide(int x, int y)
+        {
+            if ((X <= x) && (x <= X + Widht) && (Y <= y) && (y <= Y + Height))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
